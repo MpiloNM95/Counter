8ef55ea3b0979104b45b5d5013ccff0e1a165614
MpiloNM95 <mpilonmafu95@gmail.com>
2022-11-07 12:26:09 +0200

    Initial Commit

---
+++ Increment Counter/Increment_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Increment_CounterApp.swift
+//  Increment Counter
+//
+//  Created by Mpilo Mafu on 2022/11/07.
+//
+
+import SwiftUI
+
+@main
+struct Increment_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
